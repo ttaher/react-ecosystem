f51819b9922005faf02a8070664e1158fff0b437
ttaher <talat.abdelbaky@gmail.com>
2020-06-16 20:11:44 +0200

    add thunk

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
-import { removeTodo, markTodoAsCompleted } from './actions';
-import { displayAlert } from './thunks';
+import { loadTodos, removeTodoRequest, markTodoAsCompletedRequest } from './thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompeletedPressed,onDisplayAlertClicked }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompeletedPressed={onCompeletedPressed} />)}
-    </div>
-);
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+    
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm />
+            {todos.map(todo => <TodoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}/>)}
+        </div>
+    );
+    return isLoading ? loadingMessage : content;
+};
 
 const mapStateToProps = state => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
-    onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompeletedPressed: text => dispatch(markTodoAsCompleted(text)),
-    onDisplayAlertClicked: () => dispatch(displayAlert())
+    startLoadingTodos: () => dispatch(loadTodos()),
+    onRemovePressed: id => dispatch(removeTodoRequest(id)),
+    onCompletedPressed: id => dispatch(markTodoAsCompletedRequest(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({ todo, onRemovePressed, onCompeletedPressed }) => (
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
-            {
-                todo.isCompleted ? null : <button onClick={() => onCompeletedPressed(todo.text)} className="completed-button">Mark As Completed</button>
-            }
+            {todo.isCompleted
+                ? null
+                : <button
+                    onClick={() => onCompletedPressed(todo.id)}
+                    className="completed-button">Mark As Completed</button>}
             <button
-                onClick={() => onRemovePressed(todo.text)}
+                onClick={() => onRemovePressed(todo.id)}
                 className="remove-button">Remove</button>
         </div>
     </div>
