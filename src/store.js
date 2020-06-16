d801dbb67bed687b758d78c68fc5c9df5faf9034
ttaher <talat.abdelbaky@gmail.com>
2020-06-16 17:00:20 +0200

    add thunk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import thunk from 'redux-thunk';
 import { todos } from './todos/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducers = {
     todos,
@@ -16,8 +18,7 @@ const persistConfig = {
 const rootReducer = combineReducers(reducers);
 const presistedReducer = persistReducer(persistConfig, rootReducer);
 export const configureStore = () =>
- createStore(
-     presistedReducer,
-     window.__REDUX_DEVTOOLS_EXTENSION__ && 
-     window.__REDUX_DEVTOOLS_EXTENSION__(),
-     );
+    createStore(
+        presistedReducer,
+        composeWithDevTools(applyMiddleware(thunk))
+    );
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import { removeTodo, markTodoAsCompleted } from './actions';
+import { displayAlert } from './thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed ,onCompeletedPressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompeletedPressed,onDisplayAlertClicked }) => (
     <div className="list-wrapper">
         <NewTodoForm />
         {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompeletedPressed={onCompeletedPressed} />)}
@@ -18,7 +19,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompeletedPressed: text => dispatch(markTodoAsCompleted(text))
+    onCompeletedPressed: text => dispatch(markTodoAsCompleted(text)),
+    onDisplayAlertClicked: () => dispatch(displayAlert())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
