251b690c1876833362c60b84e1edf97dd51959d7
ttaher <talat.abdelbaky@gmail.com>
2020-06-16 12:26:36 +0200

    hide completed btn

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -5,7 +5,9 @@ const TodoListItem = ({ todo, onRemovePressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
-            <button className="completed-button">Mark As Completed</button>
+            {
+            todo.isCompleted ? null : <button className="completed-button">Mark As Completed</button>
+            }
             <button
                 onClick={() => onRemovePressed(todo.text)}
                 className="remove-button">Remove</button>
