0f6d5f7d1e362a4116c112367b050f9edabc7ba5
ttaher <talat.abdelbaky@gmail.com>
2020-06-16 14:26:59 +0200

    add completed

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
-import { removeTodo } from './actions';
+import { removeTodo, markTodoAsCompleted } from './actions';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed }) => (
+const TodoList = ({ todos = [], onRemovePressed ,onCompeletedPressed }) => (
     <div className="list-wrapper">
         <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />)}
+        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompeletedPressed={onCompeletedPressed} />)}
     </div>
 );
 
@@ -18,6 +18,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompeletedPressed: text => dispatch(markTodoAsCompleted(text))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({ todo, onRemovePressed }) => (
+const TodoListItem = ({ todo, onRemovePressed, onCompeletedPressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
             {
-            todo.isCompleted ? null : <button className="completed-button">Mark As Completed</button>
+                todo.isCompleted ? null : <button onClick={() => onCompeletedPressed(todo.text)} className="completed-button">Mark As Completed</button>
             }
             <button
                 onClick={() => onRemovePressed(todo.text)}
